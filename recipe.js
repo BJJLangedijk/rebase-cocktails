f2f2eefe79fc6f4db6c931c44178682325e6f64c
Bart Langedijk <bart.langedijk@vodafoneziggo.com>
2022-01-23 13:12:43 +0100

    Add base recipe

---
+++ recipe.js
@@ -0,0 +1,32 @@
+function passionFruits() {
+    return '2 ripe passion fruits (they should have a crinkly appearance), halved';
+}
+
+function vanillaVodka() {
+    return '60ml vanilla vodka';
+}
+
+function passoa() {
+    return '30ml Passoa';
+}
+
+function limeJuice() {
+    return '1 tbsp lime juice';
+}
+
+function sugarSyrup() {
+    return '1 tbsp sugar syrup';
+}
+
+function toServe() {
+    return 'Prosecco, to serve';
+}
+
+function main() {
+    passionFruits();
+    vanillaVodka();
+    passoa();
+    limeJuice();
+    sugarSyrup();
+    toServe();
+}
